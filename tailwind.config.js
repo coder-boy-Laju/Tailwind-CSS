f8c79b475bf31ec188a151dbdd5e3017194f6184
coder-boy-Laju <mdlaju43@gmail.com>
2024-08-08 02:33:58 +0600

    Add font family and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+fontFamily:{
+"manrope":["Manrope", ...defaultTheme.fontFamily.sans]
+
+},
+colors:{
+  transparent: 'transparent',
+  current: 'currentColor',
+  "Primary-color":"red",
+ "Gray-color-1": "#808080",
+ "Yellow-color1":"#FFFF00"
+},
+
+    },
   },
   plugins: [],
 }
